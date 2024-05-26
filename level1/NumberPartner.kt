c474ab9459ce9e080f0d4f7574a0c20eaa3a5e38
DaeYoung Jeong <eodud1025@naver.com>
2024-05-26 21:57:33 +0900

    level 1 숫자 짝꿍 풀이

--- level1/NumberPartner.kt
+++ level1/NumberPartner.kt
@@ -2,41 +2,35 @@ package com.example.codingtest.level1
 
 class NumberPartner {
     fun solution(X: String, Y: String): String {
-        val countsX = X.groupingBy { it }.eachCount()
-        val number = Array(10) { it.toString() }
-        var partner = mutableListOf<Int>()
-        for (i in number.indices) {
-            if (X.contains(number[i]) && Y.contains(number[i])) {
-               if (X.count { it.toString() == number[i] } > Y.count { it.toString() == number[i] } ) {
-                    repeat(Y.count { it.toString() == number[i] }) {
-                        partner.add(number[i].toInt())
-                    }
-                } else {
-                    repeat(X.count { it.toString() == number[i] }) {
-                        partner.add(number[i].toInt())
-                    }
-                }
+        val mapX = X.groupingBy { it }.eachCount()
+        val mapY = Y.groupingBy { it }.eachCount()
+        var partner = ""
+        var zero = true
+
+        for (dight in '9' downTo '1') {
+            val count = minOf(mapX.getOrDefault(dight, 0), mapY.getOrDefault(dight, 0))
+            if (count > 0) {
+                zero = false
+                partner += dight.toString().repeat(count)
             }
         }
-
-        if (partner.maxOrNull() == 0) {
-            partner = partner.distinct().toMutableList()
-            println("partner: $partner")
+        val zeroCount = minOf(mapX.getOrDefault('0', 0), mapY.getOrDefault('0', 0))
+        if (zeroCount > 0 && !zero) {
+            partner += '0'.toString().repeat(zeroCount)
+        } else if (zeroCount > 0 && zero) {
+            partner += '0'
         }
-        else if (partner.isEmpty()) partner.add(-1)
-        return partner.sortedDescending().fold("") { sum, num ->
-            sum + num
-        }
+        return partner.toString().ifEmpty { "-1" }
     }
 }
 
 
 fun main() {
     println(NumberPartner().solution("100", "2345"))
-//    println(NumberPartner().solution("100", "203045"))
-//    println(NumberPartner().solution("100", "123450"))
-//    println(NumberPartner().solution("12321", "42531"))
-//    println(NumberPartner().solution("5525", "1255"))
+    println(NumberPartner().solution("100", "203045"))
+    println(NumberPartner().solution("100", "123450"))
+    println(NumberPartner().solution("12321", "42531"))
+    println(NumberPartner().solution("5525", "1255"))
     println(NumberPartner().solution("100", "100"))
     println(NumberPartner().solution("10000", "20000"))
 
