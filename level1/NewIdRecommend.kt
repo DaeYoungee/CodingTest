06d75e95a7c87e6bf9aac77d1563a0a2b3a39f8d
DaeYoung Jeong <eodud1025@naver.com>
2024-06-10 15:48:18 +0900

    level1 신규 아이디 추천 풀이(간결한 코드)

--- level1/NewIdRecommend.kt
+++ level1/NewIdRecommend.kt
@@ -2,23 +2,17 @@ package com.example.codingtest.level1
 
 import java.util.Locale
 
+// 신규 아이디 추천
 class NewIdRecommend {
     fun solution(new_id: String): String {
 
         var step2 = new_id.toLowerCase(Locale.ROOT)
             .filter { it in 'a'..'z' || it in '0'..'9' || it == '-' || it == '_' || it == '.' }
-        while (step2.contains("..")) {
-            step2 = step2.replace("..", ".")
-        }
-        if (step2[0] == '.') {
-            step2 = step2.substring(1)
-        }
-        if (step2.isNotEmpty() && step2.last() == '.') {
-            step2 = step2.substring(0, step2.length - 1)
-        }
-        if (step2.isEmpty()) {
-            step2 = "a"
-        }
+            .replace("[.]+".toRegex(), ".")
+            .removePrefix(".")
+            .removeSuffix(".")
+            .let { if (it.isEmpty()) "a" else it }
+
         if (step2.length >= 16) {
             step2 = step2.substring(0, 15)
         } else if (step2.length <= 2) {
@@ -26,19 +20,13 @@ class NewIdRecommend {
                 step2 += step2.last()
             }
         }
-        if (step2[0] == '.') {
-            step2 = step2.substring(1)
-        }
-        if (step2.isNotEmpty() && step2.last() == '.') {
-            step2 = step2.substring(0, step2.length - 1)
-        }
-        return step2
+        return step2.removeSuffix(".")
     }
 }
 
 fun main() {
-//    println(NewIdRecommend().solution("...!@BaT#*..y.abcdefghijklm"))
-//    println(NewIdRecommend().solution("z-+.^."))
+    println(NewIdRecommend().solution("...!@BaT#*..y.abcdefghijklm"))
+    println(NewIdRecommend().solution("z-+.^."))
     println(NewIdRecommend().solution("=.="))
     println(NewIdRecommend().solution("123_.def"))
     println(NewIdRecommend().solution("abcdefghijklmn.p"))
