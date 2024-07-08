f925009c76b9c7f0a40f75ed73c7f065e2dac1b7
DaeYoung Jeong <eodud1025@naver.com>
2024-07-08 19:31:19 +0900

    level2 숫자 변환하기 풀이

--- level2/BFS/NumberConvert.kt
+++ level2/BFS/NumberConvert.kt
@@ -7,6 +7,7 @@ class NumberConvert {
     fun solution(x: Int, y: Int, n: Int): Int {
 
         val q: Queue<Int> = LinkedList()
+        val visited = hashSetOf<Int>()
         val count: Queue<Int> = LinkedList()
 
         q.add(x)
@@ -25,16 +26,20 @@ class NumberConvert {
             }
 
 
-            val list = listOf("3", "2", "$n")
+            val list = listOf("*3", "*2", "+$n")
             for (i in list) {
                 var newX = num
-                if (i == "3") {
+                if (i == "*3") {
                     newX *= 3
-                } else if (i == "2") {
+                } else if (i == "*2") {
                     newX *= 2
-                } else if (i == "$n") {
+                } else if (i == "+$n") {
                     newX += n
                 }
+                if (visited.contains(newX)) {
+                    continue
+                }
+                visited.add(newX)
                 q.add(newX)
                 count.add(c + 1)
             }
@@ -46,7 +51,7 @@ class NumberConvert {
 fun main() {
     val numberConvert = NumberConvert()
 //    println(numberConvert.solution(10, 40, 5))
-    println(numberConvert.solution(10, 40, 30))
+    println(numberConvert.solution(2, 100, 3))
 //    println(numberConvert.solution(2, 5, 4))
 
 }
